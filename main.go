606db61cda730aef0b415a2b2497aa83f118bcc6
Shaina Fischer <shainafischer@gmail.com>
2021-12-06 19:33:49 -0500

    Add docs, goreleaser, and GH actions

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/honeybeehacker/terraform-provider-testing/testing"
+	"terraform-provider-testing/testing"
 )
 
 func main() {
