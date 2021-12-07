ef126714f405295452d2d82101b5f985eaace66e
Shaina Fischer <shainafischer@gmail.com>
2021-12-06 20:09:36 -0500

    Add docs, goreleaser, and GH actions

--- main.go
+++ main.go
@@ -7,8 +7,5 @@ import (
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return provider.Provider()
-		},
-	})
+		ProviderFunc: testing.Provider})
 }
